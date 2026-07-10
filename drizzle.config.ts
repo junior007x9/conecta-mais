21f25837de7dc4a799eb7ab8e01c3e8cefa730fd
junior007x9 <santos.junior12@hotmail.com>
2026-07-10 16:25:48 -0300

    Initial commit from Create Next App

---
+++ drizzle.config.ts
@@ -0,0 +1,16 @@
+// drizzle.config.ts
+import { defineConfig } from 'drizzle-kit';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export default defineConfig({
+  schema: './db/schema.ts',
+  out: './drizzle',
+  dialect: 'sqlite',
+  driver: 'turso',
+  dbCredentials: {
+    url: process.env.TURSO_DATABASE_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN!,
+  },
+});
