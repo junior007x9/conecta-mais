3eab5beb0650df5a184ec5dba6d599e36f2b2df0
junior007x9 <santos.junior12@hotmail.com>
2026-07-17 11:22:25 -0300

    Adicionando blindagem de URL no banco de dados

--- db/index.ts
+++ db/index.ts
@@ -4,8 +4,9 @@ import { createClient } from '@libsql/client';
 import * as schema from './schema';
 
 const client = createClient({
-  url: process.env.TURSO_DATABASE_URL!,
-  authToken: process.env.TURSO_AUTH_TOKEN!,
+  // O "|| 'file:./local.db'" impede o erro de URL_INVALID durante o build da Vercel
+  url: process.env.TURSO_DATABASE_URL || 'file:./local.db',
+  authToken: process.env.TURSO_AUTH_TOKEN || '',
 });
 
 export const db = drizzle(client, { schema });
